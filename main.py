220c4e2223808fa258c6292e90878729277d2164
Dayvid Santana da Silva <santana.dayvid@outlook.com>
2023-04-01 21:34:37 -0300

    feat: open browser

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 from dataclasses import field
 import time
 import requests
-import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
-import json
 
 
-urlGruposPesquisa = 'https://computacao.ufs.br/pagina/4656'
-driver = webdriver.Chrome('')
+urlResearchGroups = 'https://computacao.ufs.br/pagina/4656'
+driver = webdriver.Chrome() 
+
+
+driver.get(urlResearchGroups)
+
+time.sleep(10)
+
+driver.quit()
