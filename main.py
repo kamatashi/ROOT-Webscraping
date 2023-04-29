658da4f59e7a385e66a3ca248f292205c38177b2
Dayvid Santana da Silva <santana.dayvid@outlook.com>
2023-04-29 19:02:41 -0300

    feat: separate functions for query on getData.py file

--- main.py
+++ main.py
@@ -1,29 +1,10 @@
-from dataclasses import field
-import requests
-import pandas as pd
-from bs4 import BeautifulSoup
-from selenium import webdriver
+from getData import openPage, createDatabase
+
 
 
 urlResearchGroups = 'https://computacao.ufs.br/pagina/4656'
 urlTest = 'https://dayvidsantana.netlify.app/'
 classTest = 'txt-present'
-
-driver = webdriver.Chrome() 
-
-def takingData(url, classLookingFor, tagHTML = 'div'):
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
-    data = soup.find(tagHTML, class_=classLookingFor)
-    return data.get_text()
-
-
-
-def openPage(url, classLookingFor, tagHTML ='div'):
-    driver.get(url)
-    data = takingData(url, classLookingFor, tagHTML)
-    driver.quit()
-    return data
 
 
 print(openPage(urlTest, classTest, 'p'))
