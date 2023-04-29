5d76d1c8e6a6f24d430c0db89151e8c1a0e75d42
Dayvid Santana da Silva <santana.dayvid@outlook.com>
2023-04-29 17:10:09 -0300

    feat: function to open browser added

--- main.py
+++ main.py
@@ -11,24 +11,11 @@ urlResearchGroups = 'https://computacao.ufs.br/pagina/4656'
 driver = webdriver.Chrome() 
 
 
-def createDataBase(way):
-    values = []
-    df = pd.DataFrame({'Values': values})
-    df.to_csv('data.csv', index=False)
-
-    # necessário caminho das DIVs no data, o way é o DATA
-    for item in way.find_all('li'):
-        values.append(item.text)
-
-
-def lookingForData():
-    soup = BeautifulSoup(driver.content, 'html.parser')
-    data = soup.find('div', class_='data')
-    return data
 
 
 def openPage(url):
     driver.get(url)
+    #lookingForData(url)
     time.sleep(10)
     driver.quit()
 
